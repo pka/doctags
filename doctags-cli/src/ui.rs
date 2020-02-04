ed6c6bdea969bf6b34147f18818dc8d8d2b537f3
Pirmin Kalberer <pka@sourcepole.ch>
2020-02-04 21:52:26 +0100

    Add states for command execution

--- doctags-cli/src/ui.rs
+++ doctags-cli/src/ui.rs
@@ -13,22 +13,67 @@ pub use crossterm::{
 use doctags::{search, Index};
 use std::io::{self, Write};
 
+#[derive(PartialEq)]
+enum CommandType {
+    Foreach,
+    Eachdir,
+}
+
+#[derive(PartialEq)]
+enum State {
+    Selecting,
+    Selected(String),
+    CommandExec(CommandType, String, Vec<String>),
+    // ShortcutSelect,
+    Keywait(Box<State>),
+    Quit,
+}
+
 pub fn ui(index: &Index) -> Result<()> {
     run(&mut io::stderr(), index)
 }
 
 fn run<W: Write>(w: &mut W, index: &Index) -> Result<()> {
-    #[derive(PartialEq)]
-    enum State {
-        Selecting,
-        Quit,
-        Selected(String),
-    }
-
     execute!(w, terminal::EnterAlternateScreen)?;
 
     terminal::enable_raw_mode()?;
 
+    let mut selected_line = None;
+    let mut state = State::Selecting;
+    while state != State::Quit {
+        state = match state {
+            State::Selecting => select(w, index)?,
+            State::CommandExec(CommandType::Foreach, command, entries) => {
+                foreach(w, command, entries)?
+            }
+            State::CommandExec(CommandType::Eachdir, command, entries) => {
+                eachdir(w, command, entries)?
+            }
+            State::Selected(line) => {
+                selected_line = Some(line);
+                State::Quit
+            }
+            State::Keywait(nextstate) => keywait(w, *nextstate)?,
+            _ => todo!(),
+        }
+    }
+
+    execute!(
+        w,
+        style::ResetColor,
+        cursor::Show,
+        terminal::LeaveAlternateScreen
+    )?;
+
+    if let Some(line) = selected_line {
+        print!("{}", line);
+    }
+
+    terminal::disable_raw_mode()?;
+    Ok(())
+}
+
+fn select<W: Write>(w: &mut W, index: &Index) -> Result<State> {
     queue!(
         w,
         SetBackgroundColor(Color::Black),
@@ -49,7 +94,6 @@ fn run<W: Write>(w: &mut W, index: &Index) -> Result<()> {
 
     w.flush()?;
 
-    let mut state = State::Selecting;
     let mut lines = Vec::new();
 
     // User input for search
@@ -58,7 +102,7 @@ fn run<W: Write>(w: &mut W, index: &Index) -> Result<()> {
 
     let (_cols, rows) = terminal::size()?;
 
-    while state == State::Selecting {
+    loop {
         if let Ok(results) = search::search_matches(index, &searchinput, (rows - 2) as usize) {
             // Ignore empty results or search errors (e.g. incomplete ':' expression)
             if results.len() > 0 {
@@ -80,11 +124,11 @@ fn run<W: Write>(w: &mut W, index: &Index) -> Result<()> {
         if let Event::Key(KeyEvent { code, modifiers }) = event::read()? {
             match code {
                 KeyCode::Esc => {
-                    state = State::Quit;
+                    return Ok(State::Quit);
                 }
                 // Ctrl-c
                 KeyCode::Char('c') if modifiers == KeyModifiers::CONTROL => {
-                    state = State::Quit;
+                    return Ok(State::Quit);
                 }
                 KeyCode::Up => {
                     if selected > 0 {
@@ -97,7 +141,7 @@ fn run<W: Write>(w: &mut W, index: &Index) -> Result<()> {
                     }
                 }
                 KeyCode::Enter => {
-                    state = State::Selected(lines[selected].text.clone());
+                    return Ok(State::Selected(lines[selected].text.clone()));
                 }
                 KeyCode::Char(ch) if modifiers.is_empty() || modifiers == KeyModifiers::SHIFT => {
                     searchinput.push(ch);
@@ -107,33 +151,96 @@ fn run<W: Write>(w: &mut W, index: &Index) -> Result<()> {
                     searchinput.pop();
                     selected = 0;
                 }
+                // Alt-f
+                KeyCode::Char('f') if modifiers == KeyModifiers::ALT => {
+                    let entries: Vec<String> = lines.iter().map(|line| line.text.clone()).collect();
+                    return Ok(enter_shell_command(w, CommandType::Foreach, entries)?);
+                }
+                // Alt-d
+                KeyCode::Char('d') if modifiers == KeyModifiers::ALT => {
+                    let entries: Vec<String> = lines.iter().map(|line| line.text.clone()).collect();
+                    return Ok(enter_shell_command(w, CommandType::Eachdir, entries)?);
+                }
                 _ => {
                     // println!("{}", format!("OTHER InputEvent: {:?}\n\n", code));
                 }
             }
         }
     }
+}
 
-    execute!(
+fn enter_shell_command<W: Write>(
+    w: &mut W,
+    cmdtype: CommandType,
+    entries: Vec<String>,
+) -> Result<State> {
+    queue!(
         w,
+        cursor::MoveTo(0, 1),
         style::ResetColor,
-        cursor::Show,
-        terminal::LeaveAlternateScreen
+        SetBackgroundColor(Color::Black),
+        Print("Command: ")
     )?;
+    w.flush()?;
+    let command = "echo".to_string();
+    Ok(State::CommandExec(cmdtype, command, entries))
+}
 
-    match state {
-        State::Selected(line) => {
-            print!("{}", line);
-        }
-        _ => {}
-    }
-
+fn foreach<W: Write>(w: &mut W, command: String, entries: Vec<String>) -> Result<State> {
+    queue!(
+        w,
+        cursor::MoveTo(0, 2),
+        style::ResetColor,
+        SetBackgroundColor(Color::Black),
+        terminal::Clear(ClearType::FromCursorDown)
+    )?;
+    w.flush()?;
     terminal::disable_raw_mode()?;
-    Ok(())
+    dbg!(command, entries); //TODO
+    terminal::enable_raw_mode()?;
+    Ok(State::Keywait(Box::new(State::Selecting)))
+}
+
+fn eachdir<W: Write>(w: &mut W, command: String, entries: Vec<String>) -> Result<State> {
+    queue!(
+        w,
+        cursor::MoveTo(0, 2),
+        style::ResetColor,
+        SetBackgroundColor(Color::Black),
+        terminal::Clear(ClearType::FromCursorDown)
+    )?;
+    w.flush()?;
+    terminal::disable_raw_mode()?;
+    dbg!(command, entries); //TODO
+    terminal::enable_raw_mode()?;
+    Ok(State::Keywait(Box::new(State::Selecting)))
+}
+
+fn keywait<W: Write>(_w: &mut W, next_state: State) -> Result<State> {
+    let mut next = next_state;
+    if let Event::Key(KeyEvent { code, modifiers }) = event::read()? {
+        next = match code {
+            KeyCode::Esc => State::Quit,
+            // Ctrl-c
+            KeyCode::Char('c') if modifiers == KeyModifiers::CONTROL => State::Quit,
+            _ => next,
+        };
+    }
+    Ok(next)
 }
 
 fn print_menu<W: Write>(w: &mut W) -> Result<()> {
-    let entries = [("ESC", "quit"), ("Enter", "select")];
+    let entries = [
+        ("ESC", "quit"),
+        ("Enter", "select"),
+        // ("Alt-v", "view"),
+        // ("Alt-o", "open"),
+        // ("Alt-p", "open folder"),
+        ("Alt-f", "foreach"),
+        ("Alt-d", "eachdir"),
+        // ("Alt-s", "shortcut"),
+        // ("Alt-c", "edit config"),
+    ];
     let menu_normal = Color::AnsiValue(252);
     let menu_command = Color::AnsiValue(220);
     let menu_background = Color::AnsiValue(235);
